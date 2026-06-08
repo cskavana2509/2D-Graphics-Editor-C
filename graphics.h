898211d5564706c75e5675f9b7469008ca2784f4
cskavana2509 <ugcet2502676@reva.edu.in>
2026-06-08 11:01:28 +0000

    Initial graphics editor

--- graphics.h
+++ graphics.h
@@ -6,7 +6,12 @@
 
 extern char canvas[ROWS][COLS];
 
-void initCanvas();
+void initializeCanvas();
 void displayCanvas();
+
+void drawLine(int x1,int y1,int x2,int y2);
+void drawRectangle(int x,int y,int width,int height);
+void drawTriangle(int x,int y,int size);
+void drawCircle(int xc,int yc,int r);
 
 #endif
--- main.c
+++ main.c
@@ -3,8 +3,58 @@
 
 int main()
 {
-    initCanvas();
-    displayCanvas();
+    int choice;
+
+    initializeCanvas();
+
+    do
+    {
+        printf("\n===== 2D GRAPHICS EDITOR =====\n");
+        printf("1. Draw Line\n");
+        printf("2. Draw Rectangle\n");
+        printf("3. Draw Triangle\n");
+        printf("4. Draw Circle\n");
+        printf("5. Display Picture\n");
+        printf("6. Exit\n");
+
+        printf("Enter choice: ");
+        scanf("%d",&choice);
+
+        switch(choice)
+        {
+            case 1:
+                drawLine(5,5,20,5);
+                printf("Line Drawn\n");
+                break;
+
+            case 2:
+                drawRectangle(10,10,15,6);
+                printf("Rectangle Drawn\n");
+                break;
+
+            case 3:
+                drawTriangle(30,5,6);
+                printf("Triangle Drawn\n");
+                break;
+
+            case 4:
+                drawCircle(40,20,5);
+                printf("Circle Drawn\n");
+                break;
+
+            case 5:
+                displayCanvas();
+                break;
+
+            case 6:
+                printf("Exiting...\n");
+                break;
+
+            default:
+                printf("Invalid Choice\n");
+        }
+
+    } while(choice!=6);
 
     return 0;
 }
